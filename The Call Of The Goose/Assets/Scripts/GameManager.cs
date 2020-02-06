9d9a9f9377d618ab662fdb0e4decdc00e92069f8
Yvon Morice <yvon.morice@epita.fr>
2020-02-06 22:56:29 +0100

    Added Project files

---
+++ The Call Of The Goose/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
