4d4ec5d832c1b8b0704bac31434a8ea9c76c225a
Yvon Morice <yvon.morice@epita.fr>
2020-04-22 14:18:42 +0200

    Animations

--- The Call Of The Goose/Assets/Scripts/Entities/PlayerMovement.cs
+++ The Call Of The Goose/Assets/Scripts/Entities/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Entities
 {
@@ -7,17 +8,42 @@ namespace Entities
         public float moveSpeed = 5f;
 
         public Rigidbody2D rb;
+        public Animator Animator;
 
         private Vector2 _movement;
+        private static readonly int Running = Animator.StringToHash("Running");
+        private static readonly int Left = Animator.StringToHash("Left");
+        private static readonly int Right = Animator.StringToHash("Right");
+
         void Update()
         {
-            _movement.x = Input.GetAxisRaw("Horizontal");
-            _movement.y = Input.GetAxisRaw("Vertical");
+            _movement.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            _movement.y = Input.GetAxisRaw("Vertical") * moveSpeed;
+            
+            if (Math.Abs(_movement.x) > 0f || Math.Abs(_movement.y) > 0f)
+                Animator.SetBool(Running, true);
+            else Animator.SetBool(Running, false);
+            
+            if (_movement.x > 0)
+            {
+                Animator.SetBool(Right, true);
+                Animator.SetBool(Left, false);
+            }
+            else if (_movement.x < 0)
+            {
+                Animator.SetBool(Left, true);
+                Animator.SetBool(Right, false);
+            }
+            else
+            {
+                Animator.SetBool(Right, false);
+                Animator.SetBool(Left, false);
+            }
         }
 
         private void FixedUpdate()
         {
-            rb.MovePosition(rb.position + Time.fixedDeltaTime * moveSpeed * _movement);
+            rb.MovePosition(rb.position + _movement * Time.fixedDeltaTime);
         }
     }
 }
