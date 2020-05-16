4cc2b1cfac5858c135f789653105209add9e86b2
Souleymane Sentici <souleymane.sentici@epita.fr>
2020-05-16 15:54:56 +0200

    class inventory et use de consommable

--- The Call Of The Goose/Assets/Scripts/Item/Consumable.cs
+++ The Call Of The Goose/Assets/Scripts/Item/Consumable.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using Entities;
+using UnityEngine;
 
 namespace Item
 {
     [CreateAssetMenu (fileName = "new Consumable", menuName = "Items/Consumable")]
     public class Consumable : Item
     {
+        public int heal = 0;
         
+        public override void Use()
+        {
+            GameObject player = Inventory.instance.player;
+            
+        }
     }
 }
